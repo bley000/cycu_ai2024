ecd4425067231da8637a3e691b6fbd1ea8e50fad
張家瑋 <160596364+bley000@users.noreply.github.com>
2024-03-05 23:01:33 +0800

    2

--- 20240305/oil_2.py
+++ 20240305/oil_2.py
@@ -6,7 +6,7 @@ def get_data(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     tables = soup.find_all('table')
-    data_frames = [pd.read_html(str(table))[0] for table in tables]
+    data_frames = [pd.read_html(str(tables[1]))[0]]
     return pd.concat(data_frames)
 
 url1 = 'https://vipmbr.cpc.com.tw/mbwebs/ShowHistoryPrice_oil.aspx'
@@ -15,33 +15,24 @@ url2 = 'https://vipmbr.cpc.com.tw/mbwebs/ShowHistoryPrice_oil2019.aspx'
 data1 = get_data(url1)
 data2 = get_data(url2)
 
+#將data1和data2合併
 data = pd.concat([data1, data2])
-
 data.to_csv('C:/Users/USER/Desktop/oil_prices.csv', index=False)
-print(data)
-#將NAN值去除
-data = data.dropna()
-#只保留前5個欄位的資料
+#只留下data前五個欄位
 data = data.iloc[:, :5]
 
+#去除值是NaN的資料
+data = data.dropna()
 
-
-#將第一欄的資料型態轉成datetime
-data[data.columns[0]] = pd.to_datetime(data[data.columns[0]])
-print(data)
-
-#以matplotlib繪製折線圖 以日期為x軸 油價為y軸
+#以matplotlib繪製折線圖
 import matplotlib.pyplot as plt
-#設定中文字型
+
+#文字修正
 plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
 plt.rcParams['axes.unicode_minus'] = False
-# 使用 matplotlib x,y 折線圖 , x 軸是日期 , 後面四個欄位是 油價 ，分別是 92無鉛汽油,95無鉛汽油,98無鉛汽油,超級柴油 
-plt.figure(figsize=(60, 6))
-plt.plot(data[data.columns[0]], data[data.columns[1]], label='92無鉛汽油')
-plt.plot(data[data.columns[0]], data[data.columns[2]], label='95無鉛汽油')
-plt.plot(data[data.columns[0]], data[data.columns[3]], label='98無鉛汽油')
-plt.plot(data[data.columns[0]], data[data.columns[4]], label='超級柴油')
-plt.legend()
+
+#設定index
+data.set_index(data.columns[0], inplace=True)
+data.plot(figsize=(20, 6))  # 在這裡調整圖表大小
 plt.xticks(rotation=45)
-plt.xticks(data[data.columns[0]][::30])
 plt.show()
